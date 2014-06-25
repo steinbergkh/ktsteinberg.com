425ce720fc619a98fb53af0b63e7a2b63e4bcc6d
steinbergkh <csteinb@umich.edu>
2014-06-24 21:12:32 -0400

    Added grunt forever

---
+++ starter.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
+then
+        export PATH=/usr/local/bin:$PATH
+        grunt serve  >> /var/www/npmdebug.txt 2>&1
+fi
+
